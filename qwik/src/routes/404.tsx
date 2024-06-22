288c4a9d09cd276bc1bebbd553eaae13c0530a44
upayanmazumder <upayanm3@gmail.com>
2024-06-22 13:42:38 +0530

    🛠️ router-head.tsx -> Added import for friendsfun image 🛠️ router-head.tsx -> Updated title and og:image handling 🔴 404.tsx

--- qwik/src/routes/404.tsx
+++ qwik/src/routes/404.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import SecondaryFooter from "~/components/secondary-footer/secondary-footer";
 
 export default component$(() => {
   return (
@@ -8,6 +9,7 @@ export default component$(() => {
         <h1>404 Not Found</h1>
         <p>The page you are looking for does not exist.</p>
       </div>
+      <SecondaryFooter/>
     </>
   );
 });
