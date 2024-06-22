21b4f5fa2a53aa83434041f260238778c0bdcea7
upayanmazumder <upayanm3@gmail.com>
2024-06-22 13:45:29 +0530

    fix

--- qwik/src/routes/404.tsx
+++ qwik/src/routes/404.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import SecondaryFooter from "~/components/secondary-footer/secondary-footer";
+import SecondaryFooter from "../components/secondary-footer/secondary-footer";
 
 export default component$(() => {
   return (
